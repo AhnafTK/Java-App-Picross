4dd716f69b6842ab372d7de21b97ac1c563ebb91
Skylar898 <122387513+Skylar898@users.noreply.github.com>
2023-01-24 16:09:07 -0800

    Game layout update

--- Game.java
+++ Game.java
@@ -1,12 +1,6 @@
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
-
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Game {
 	
@@ -17,7 +11,6 @@ public class Game {
 	
 	
 	public static void main (String[] args) {
-				
 		JFrame picrossWindow = new JFrame();
 		JButton button = new JButton();
 		JCheckBox checkBox = new JCheckBox();
@@ -27,46 +20,56 @@ public class Game {
 		//butGame[N][N]
 		
 		//TODO: Make a "How to play" button that has a window popup of instructions
-		GridLayout gridLayout = new GridLayout(3,3);
-		picrossWindow.setLayout(gridLayout);
 		
-		//Board Panel
-		JPanel boardPanel = new JPanel();
-		boardPanel.setBackground(Color.RED);
 		
-		//Mark Panel
+		JPanel titlePanel = new JPanel();
+		titlePanel.setBackground(Color.RED);
+		titlePanel.setPreferredSize(new Dimension (1000, 125));
+		//TODO: ImageIcon
+		//TODO: Image
+		
+		
 		JPanel markPanel = new JPanel();
 		markPanel.setBackground(Color.BLACK);
+		markPanel.setPreferredSize(new Dimension (100, 100));
 
-		//Top Panel
-		JPanel topPanel = new JPanel();
-		topPanel.setBackground(Color.GREEN);
-		
-		//Title Panel
-		JPanel titlePanel = new JPanel();
-		titlePanel.setBackground(Color.ORANGE);
-		
-		//Left Panel
 		JPanel leftPanel = new JPanel();
 		leftPanel.setBackground(Color.BLUE);
+		leftPanel.setPreferredSize(new Dimension (250, 200));
 		
-		//Control Panel
 		JPanel controlPanel = new JPanel();
+		controlPanel.setBackground(Color.ORANGE);
+		controlPanel.setPreferredSize(new Dimension (250, 200));
+		
+		JPanel boardPanel = new JPanel();
+		boardPanel.setBackground(Color.GREEN);
+		boardPanel.setLayout(new GridLayout(5,5));
+		
+		JButton[] buttons = new JButton[25];
+		for(int i=0;i<25;i++) {
+			buttons[i] = new JButton();
+			buttons[i].setPreferredSize(new Dimension(50,50));
+			boardPanel.add(buttons[i]);
+			//buttons[i].setFont(new Font("MV Boli",Font.BOLD,120));
 
-		picrossWindow.add(boardPanel);
-		picrossWindow.add(markPanel);
-		picrossWindow.add(topPanel);
-		picrossWindow.add(titlePanel);
-		picrossWindow.add(leftPanel);
-		picrossWindow.add(controlPanel);
+			buttons[i].setFocusable(false);
+			
+			//buttons[i].addActionListener(this);
+			
+		}
+		
+		picrossWindow.setLayout(new BorderLayout());
+		picrossWindow.add(titlePanel, BorderLayout.NORTH);
+		picrossWindow.add(leftPanel, BorderLayout.WEST);
+		picrossWindow.add(controlPanel, BorderLayout.EAST);
+		picrossWindow.add(boardPanel, BorderLayout.CENTER);
 		picrossWindow.pack();
 		
-		//frame.setLayout(new GridLayout(5, 5));
 		picrossWindow.setResizable(false);
 		picrossWindow.setVisible(true);
 		picrossWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		picrossWindow.setTitle("Bobo");
-		picrossWindow.setSize(800, 600);
+		picrossWindow.setSize(1000, 600);
 		picrossWindow.setLocationRelativeTo(null);
 	}
 }
