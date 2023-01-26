edd26daaa405b07dbdb7e777e9977868dad3c74a
AhnafTK <ahnaf.kamal@outlook.com>
2023-01-25 21:43:36 -0500

    Added buttons for left panel. Added TODOs. Added JLabels for testing the JPanels and the visibility of their components

--- Game.java
+++ Game.java
@@ -19,6 +19,15 @@ public class Game {
 		//JLabel labRows = new JLabel();
 		//butGame[N][N]
 		
+		// RESET button for left panel.
+		JButton resetButton = new JButton();
+		resetButton.setText("RESET");
+		JButton solveButton = new JButton();
+		solveButton.setText("SOLVE");
+		JButton newBoardButton = new JButton();
+		newBoardButton.setText("NEW BOARD");
+		JButton instructionsButton = new JButton();
+		instructionsButton.setText("INSTRUCTIONS");
 		//TODO: Make a "How to play" button that has a window popup of instructions
 		
 		
@@ -34,10 +43,33 @@ public class Game {
 		markPanel.setPreferredSize(new Dimension (100, 100));
 
 		JPanel leftPanel = new JPanel();
-		leftPanel.setBackground(Color.BLUE);
+		// temp
+		JLabel leftPanelText = new JLabel();
+		leftPanelText.setText("LEFT PANEL"); // temp
+		leftPanel.add(leftPanelText);
+		//
+		
+		// adding buttons to left panel
+		leftPanel.add(resetButton);
+		leftPanel.add(solveButton);
+		leftPanel.add(newBoardButton);
+		leftPanel.add(instructionsButton);
+		/**
+		 * TODO: add score
+		 * 	   : add time
+		 * 	   : Language section
+		 */
+		
+		
+		leftPanel.setBackground(Color.GRAY);
 		leftPanel.setPreferredSize(new Dimension (250, 200));
 		
 		JPanel controlPanel = new JPanel();
+		// temp
+		JLabel controlPanelText = new JLabel();
+		controlPanelText.setText("CONTROL PANEL"); // temp
+		controlPanel.add(controlPanelText);
+		//
 		controlPanel.setBackground(Color.ORANGE);
 		controlPanel.setPreferredSize(new Dimension (250, 200));
 		
@@ -45,7 +77,7 @@ public class Game {
 		boardPanel.setBackground(Color.GREEN);
 		boardPanel.setLayout(new GridLayout(5,5));
 		
-		JButton[] buttons = new JButton[25];
+		JButton[] buttons = new JButton[25]; // 25 will be the dim^2
 		for(int i=0;i<25;i++) {
 			buttons[i] = new JButton();
 			buttons[i].setPreferredSize(new Dimension(50,50));
