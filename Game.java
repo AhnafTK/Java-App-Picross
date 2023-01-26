b287c6c0b905a6f098f4d67479176a6aa54d6028
AhnafTK <ahnaf.kamal@outlook.com>
2023-01-26 09:39:30 -0500

    organized code, added some labels

--- Game.java
+++ Game.java
@@ -9,40 +9,66 @@ public class Game {
 		
 	}
 	
+	//TODO: Use a layout for the components of the left panel
 	
 	public static void main (String[] args) {
+		
+		// JPanel intializations
+		JPanel titlePanel = new JPanel();
+		JPanel languagePanel = new JPanel();
+		JPanel markPanel = new JPanel();
+		JPanel leftPanel = new JPanel();
+		JPanel controlPanel = new JPanel();
+		JPanel boardPanel = new JPanel();
+
+
+		// JButton intializations
+		JButton[] buttons = new JButton[25]; // 25 will be the dim^2
+		JButton resetButton = new JButton();
+		JButton solveButton = new JButton();
+		JButton newBoardButton = new JButton();
+		JButton instructionsButton = new JButton();
+		
+		// JButton config
+		resetButton.setText("RESET");
+		solveButton.setText("SOLVE");
+		newBoardButton.setText("NEW BOARD");
+		instructionsButton.setText("INSTRUCTIONS");
+
+		// JLabel init
+		JLabel timerText = new JLabel();
+		JLabel scoreText = new JLabel();
+
+		// JLabel config
+		scoreText.setText("SCORE: ");
+		timerText.setText("TIMER: ");
+		
 		JFrame picrossWindow = new JFrame();
 		JButton button = new JButton();
 		JCheckBox checkBox = new JCheckBox();
-		JLabel label = new JLabel();
+		
+			
 		//JLabel labCols = new JLabel();
 		//JLabel labRows = new JLabel();
 		//butGame[N][N]
 		
 		// RESET button for left panel.
-		JButton resetButton = new JButton();
-		resetButton.setText("RESET");
-		JButton solveButton = new JButton();
-		solveButton.setText("SOLVE");
-		JButton newBoardButton = new JButton();
-		newBoardButton.setText("NEW BOARD");
-		JButton instructionsButton = new JButton();
-		instructionsButton.setText("INSTRUCTIONS");
+
 		//TODO: Make a "How to play" button that has a window popup of instructions
 		
 		
-		JPanel titlePanel = new JPanel();
 		titlePanel.setBackground(Color.RED);
 		titlePanel.setPreferredSize(new Dimension (1000, 125));
+		
+		languagePanel.setSize(new Dimension(100,100));
+		languagePanel.setBackground(Color.GREEN);
 		//TODO: ImageIcon
 		//TODO: Image
 		
-		
-		JPanel markPanel = new JPanel();
+	
 		markPanel.setBackground(Color.BLACK);
 		markPanel.setPreferredSize(new Dimension (100, 100));
 
-		JPanel leftPanel = new JPanel();
 		// temp
 		JLabel leftPanelText = new JLabel();
 		leftPanelText.setText("LEFT PANEL"); // temp
@@ -50,10 +76,13 @@ public class Game {
 		//
 		
 		// adding buttons to left panel
+		leftPanel.add(scoreText);
+		leftPanel.add(timerText);
 		leftPanel.add(resetButton);
 		leftPanel.add(solveButton);
 		leftPanel.add(newBoardButton);
 		leftPanel.add(instructionsButton);
+		leftPanel.add(languagePanel);
 		/**
 		 * TODO: add score
 		 * 	   : add time
@@ -64,7 +93,6 @@ public class Game {
 		leftPanel.setBackground(Color.GRAY);
 		leftPanel.setPreferredSize(new Dimension (250, 200));
 		
-		JPanel controlPanel = new JPanel();
 		// temp
 		JLabel controlPanelText = new JLabel();
 		controlPanelText.setText("CONTROL PANEL"); // temp
@@ -73,11 +101,9 @@ public class Game {
 		controlPanel.setBackground(Color.ORANGE);
 		controlPanel.setPreferredSize(new Dimension (250, 200));
 		
-		JPanel boardPanel = new JPanel();
 		boardPanel.setBackground(Color.GREEN);
 		boardPanel.setLayout(new GridLayout(5,5));
 		
-		JButton[] buttons = new JButton[25]; // 25 will be the dim^2
 		for(int i=0;i<25;i++) {
 			buttons[i] = new JButton();
 			buttons[i].setPreferredSize(new Dimension(50,50));
